9c1bf3cd13682ee07df88261d1bbb5b1a556d6c0
alefari <ale.jf09@gmail.com>
2021-02-08 22:40:40 -0400

    icono, version y deployment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAfZ3Ifx5NY61sEmUyCZhENxn-EAv43Q3Y",
+    authDomain: "calculadora-becas.firebaseapp.com",
+    databaseURL: "https://calculadora-becas.firebaseio.com",
+    projectId: "calculadora-becas",
+    storageBucket: "calculadora-becas.appspot.com",
+    messagingSenderId: "928418518416",
+    appId: "1:928418518416:web:e45146abca652fecb74f15",
+    measurementId: "G-14VELXFV1R"
+  }
 };
